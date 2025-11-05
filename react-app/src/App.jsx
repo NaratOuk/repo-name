eb2179089f1a8603b77a2e7504e77d63762c2912
Narak <vuthnarak2005@gmail.com>
2025-11-05 15:59:50 +0700

    Switch to HashRouter for GitHub Pages

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,12 +4,12 @@ import Login from './page/login_webpage .jsx'
 import HomePage from './page/homepage'
 import  Signup  from './page/Signup_page.jsx'
 import { ForgotPasswordPage, ForgotPasswordPage1 } from './page/Forgotpassword-page.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/home" element={<HomePage />} />
@@ -18,7 +18,7 @@ function App() {
           <Route path='/forgot' element={<ForgotPasswordPage />} />
           <Route path='/forgot1' element={<ForgotPasswordPage1 />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
